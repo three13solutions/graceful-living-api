f65be798b0d4e03fd0ccd953845501ca09018e89
three13solutions <admin@313s.com>
2025-06-24 21:17:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import applicationRoutes from "./routes/application.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/applications", applicationRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Graceful Living API is running.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
