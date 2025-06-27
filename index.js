a5b31fa56b93eb0396c0d8ac128773b205088129
three13solutions <admin@313s.com>
2025-06-27 19:04:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import applicationRoutes from "./routes/application.js";
+import applicationRoutes from './routes/application.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/api', applicationRoutes);
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/applications", applicationRoutes);
